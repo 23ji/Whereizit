fe09490bd733f5fc44b4ae0fba3eac643c4cad2f
23ji <isj4194@naver.com>
2025-02-13 16:56:12 +0900

    뷰 수정

--- SmokingAreaOdi/View/AddSmokingAreaView.swift
+++ SmokingAreaOdi/View/AddSmokingAreaView.swift
@@ -11,19 +11,19 @@ import NMapsMap
 
 class AddSmokingAreaView: UIView {
     
-    var addSmokingArea = AddSmokeAreaViewController()
-
-    private func setupNaverMapView() {
-        let initialLocation = NMGLatLng(lat: 37.500920152198, lng: 127.03618231961)
-        let cameraUpdate = NMFCameraUpdate(scrollTo: initialLocation)
-        naverMapView.mapView.moveCamera(cameraUpdate)
-        naverMapView.showLocationButton = true
-    }
-
-    private func setupUI() {
-        searchBar.searchTextField.borderStyle = .none
-        searchBar.layer.cornerRadius = 15
-        searchBar.clipsToBounds = true
-    }
+//    var addSmokingArea = AddSmokeAreaViewController()
+//
+//    private func setupNaverMapView() {
+//        let initialLocation = NMGLatLng(lat: 37.500920152198, lng: 127.03618231961)
+//        let cameraUpdate = NMFCameraUpdate(scrollTo: initialLocation)
+//        naverMapView.mapView.moveCamera(cameraUpdate)
+//        naverMapView.showLocationButton = true
+//    }
+//
+//    private func setupUI() {
+//        searchBar.searchTextField.borderStyle = .none
+//        searchBar.layer.cornerRadius = 15
+//        searchBar.clipsToBounds = true
+//    }
 
 }
