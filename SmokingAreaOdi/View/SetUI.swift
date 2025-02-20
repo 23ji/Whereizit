7302d51cc2c4f15c235689bc7576fad1f59f31d4
23ji <isj4194@naver.com>
2025-02-20 17:15:58 +0900

    파일 정리

---
+++ SmokingAreaOdi/View/SetUI.swift
@@ -0,0 +1,16 @@
+//
+//  SetUI.swift
+//  SmokingAreaOdi
+//
+//  Created by 이상지 on 2/20/25.
+//
+
+import Foundation
+import NMapsMap
+
+weak var naverMapView: NMFNaverMapView!
+
+class setUI{
+    
+}
+
