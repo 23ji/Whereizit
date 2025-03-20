71fc352bdb8980a9831d957fa0678346b89df7a4
23ji <isj4194@naver.com>
2025-03-20 16:27:28 +0900

    첫 화면 새로 생성 : 로그인 화면

---
+++ SmokingAreaOdi/Controller/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  SmokingAreaOdi
+//
+//  Created by 이상지 on 3/20/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- SmokingAreaOdi/SceneDelegate.swift
+++ SmokingAreaOdi/SceneDelegate.swift
@@ -13,11 +13,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+            guard let _ = (scene as? UIWindowScene) else { return }
+
+            // 스토리보드에서 LoginViewController를 인스턴스화합니다.
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            // 옵셔널 바인딩을 통해 LoginViewController를 안전하게 인스턴스화합니다.
+            if let loginVC = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as? LoginViewController {
+                window?.rootViewController = loginVC
+                window?.makeKeyAndVisible()
+            }
+        }
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
