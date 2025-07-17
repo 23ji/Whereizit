3e87eb27caaaf2910fe19389d3ecfdf1660548a9
23ji <isj4194@naver.com>
2025-07-17 13:46:59 +0900

    nextButton 생성

--- SmokingAreaOdi/Controller/AddViewController.swift
+++ SmokingAreaOdi/Controller/AddViewController.swift
@@ -20,12 +20,19 @@ class AddViewController: UIViewController, NMFMapViewCameraDelegate {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.navigationItem.title = "흡연구역 추가"
+    self.navigationItem.title = "위치 지정"
     
     //카메라 델리게이트 등록해야함
     addView.mapView.addCameraDelegate(delegate: self)
     // 마커 기본 속성(특히 첫 위치 지정해줘야 나타남)
-      marker.position = addView.mapView.cameraPosition.target // 카메라 중앙에 마커 첫 위치 지정
-      marker.mapView = addView.mapView //지도에 올리기
+    marker.position = addView.mapView.cameraPosition.target // 카메라 중앙에 마커 첫 위치 지정
+    marker.mapView = addView.mapView //지도에 올리기
+    
+    addView.nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+  }
+  
+  @objc private func didTapNextButton() {
+    let detailVC = DetailViewController()
+    navigationController?.pushViewController(detailVC, animated: true)
   }
 }
--- SmokingAreaOdi/View/AddView.swift
+++ SmokingAreaOdi/View/AddView.swift
@@ -10,8 +10,8 @@ import UIKit
 
 final class AddView: UIView {
     let mapView = NMFMapView()
-    let addButton = UIButton()
-    
+    let nextButton = UIButton()
+  
     
     // 초기화 메서드 (코드로 UI 작성 시 필수)
     override init(frame: CGRect) {
@@ -30,7 +30,38 @@ final class AddView: UIView {
       //지도
       mapView.frame = bounds
       mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight] //크기 자동 조절
-      addSubview(mapView)
+      self.addSubview(mapView)
+      
+      // next 버튼
+      self.addSubview(nextButton)
+      nextButton.translatesAutoresizingMaskIntoConstraints = false //AutoLayout 위해
+      
+      NSLayoutConstraint.activate([
+        nextButton.widthAnchor.constraint(equalToConstant: 100),
+        nextButton.heightAnchor.constraint(equalToConstant: 56),
+        nextButton.centerXAnchor.constraint(equalTo: centerXAnchor),
+        nextButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -48)
+      ])
+
+      
+      let config = UIImage.SymbolConfiguration(pointSize: 24, weight: .bold)
+      //이렇게 하면 안됨
+      //nextButton.titleLabel?.text = "다음"
+      //nextButton.titleLabel?.textColor = .white
+      
+      nextButton.setTitle("다음", for: .normal) // 타이틀 설정
+      nextButton.setTitleColor(.white, for: .normal) // 텍스트 색상 설정
+      nextButton.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+      nextButton.tintColor = .white
+      nextButton.backgroundColor = .systemGreen
+      nextButton.layer.cornerRadius = 28 // 지름 56이라 28이면 완전 원 됨
+      nextButton.clipsToBounds = true
+      
+      //그림자 효과
+      nextButton.layer.shadowColor = UIColor.black.cgColor
+      nextButton.layer.shadowOpacity = 0.3
+      nextButton.layer.shadowOffset = CGSize(width: 0, height: 3)
+      nextButton.layer.shadowRadius = 4
     }
   
   func setMarker() {
