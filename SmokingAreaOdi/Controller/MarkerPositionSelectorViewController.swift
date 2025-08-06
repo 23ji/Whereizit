0a096e1100f0d9bf86de980187f753305259a38b
23ji <isj4194@naver.com>
2025-08-06 15:11:52 +0900

    리팩토링

--- SmokingAreaOdi/Controller/MarkerPositionSelectorViewController.swift
+++ SmokingAreaOdi/Controller/MarkerPositionSelectorViewController.swift
@@ -5,38 +5,57 @@
 //  Created by 이상지 on 7/16/25.
 //
 import NMapsMap
+import PinLayout
 
 import UIKit
 
 
 class MarkerPositionSelectorViewController: UIViewController, NMFMapViewCameraDelegate {
   
-  private let addView = AddView()
-  let marker = NMFMarker()
-  
-  override func loadView() {
-    view = addView
+  // MARK: Constant
+
+  private enum Metric {
+    //static let
   }
   
+  //MARK: UI
+  
+  private let mapView = NMFMapView() // 현재 위치로 초기 로케이션 세팅
+
+  //private let addView = AddView()
+  let marker = NMFMarker()
+  let nextButton = UIButton()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.addSubViews()
+    self.configure()
+    
     self.navigationItem.title = "위치 지정"
     
     //카메라 델리게이트 등록해야함
-    addView.mapView.addCameraDelegate(delegate: self)
+    self.mapView.addCameraDelegate(delegate: self)
     // 마커 기본 속성(특히 첫 위치 지정해줘야 나타남)
-    marker.position = addView.mapView.cameraPosition.target // 카메라 중앙에 마커 첫 위치 지정
-    marker.mapView = addView.mapView //지도에 올리기
+    marker.position = self.mapView.cameraPosition.target // 카메라 중앙에 마커 첫 위치 지정
+    marker.mapView = self.mapView //지도에 올리기
     
-    addView.nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+    self.nextButton.addTarget(self, action: #selector(didTapNextButton), for: .touchUpInside)
+  }
+  
+  private func addSubViews() {
+    self.view.addSubview(mapView)
+  }
+  
+  private func configure() {
+    self.mapView.pin.all()
   }
   
   @objc private func didTapNextButton() {
-    let detailVC = MarkerInfoInputViewController()
-    navigationController?.pushViewController(detailVC, animated: true)
+    let markerInfoInputVC = MarkerInfoInputViewController()
+    navigationController?.pushViewController(markerInfoInputVC, animated: true)
     
-    let center = addView.mapView.cameraPosition.target
-    detailVC.lat = center.lat
-    detailVC.lng = center.lng
+    let center = self.mapView.cameraPosition.target
+    markerInfoInputVC.lat = center.lat
+    markerInfoInputVC.lng = center.lng
   }
 }
