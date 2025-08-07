f1202813ee992eddfbef5f1e6da4269a716e149c
23ji <isj4194@naver.com>
2025-08-07 14:55:04 +0900

    섹션별로 정리

--- SmokingAreaOdi/Controller/MarkerInfoInputViewController.swift
+++ SmokingAreaOdi/Controller/MarkerInfoInputViewController.swift
@@ -12,68 +12,104 @@ import UIKit
 
 class MarkerInfoInputViewController: UIViewController {
   
+  // MARK: Constant
+  
+  private enum Metric {
+    static let mapHeight: CGFloat = 300
+    static let labelFontSize: CGFloat = 16
+    static let labelHeight: CGFloat = 50
+    static let textFieldHeight: CGFloat = 40
+    static let horizontalMargin: CGFloat = 20
+  }
+  
+  // MARK: UI
+  
   private let mapView = NMFMapView()
   private let nameLabel = UILabel()
   private let nameTextField = UITextField()
   private let descriptionLabel = UILabel()
   private let descriptionTextField = UITextField()
+  
+  // MARK: Properties
+  
   var lat: Double?
   var lng: Double?
   
+  // MARK: Lifecycle
   
   override func viewDidLoad() {
     super.viewDidLoad()
     print("내 마커 - 위도 : \(String(describing: lat)) 경도 : \(String(describing: lng))")
     self.setUI()
-    self.defineFlexContainer()
     self.setupInputs()
+    self.defineFlexContainer()
   }
   
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    self.view.flex.layout(mode: .fitContainer)
+  }
+  
+  // MARK: UI Setup
   
   private func setUI() {
     self.navigationItem.title = "흡연구역 등록"
     self.view.backgroundColor = .white
   }
   
-  
   private func setupInputs() {
     self.nameLabel.text = "흡연구역 이름"
-    self.nameLabel.font = .systemFont(ofSize: 16, weight: .bold)
+    self.nameLabel.font = .systemFont(ofSize: Metric.labelFontSize, weight: .bold)
     
     self.nameTextField.placeholder = "강남역 11번 출구"
     self.nameTextField.borderStyle = .roundedRect
     
     self.descriptionLabel.text = "흡연구역 설명"
-    self.descriptionLabel.font = .systemFont(ofSize: 16, weight: .bold)
+    self.descriptionLabel.font = .systemFont(ofSize: Metric.labelFontSize, weight: .bold)
   
     self.descriptionTextField.placeholder = "우측으로 5m"
     self.descriptionTextField.borderStyle = .roundedRect
   }
   
+  // MARK: Layout
   
   private func defineFlexContainer() {
     self.view.flex.addItem()
       .direction(.column)
       .alignItems(.stretch)
       .define {
-        $0.addItem(self.mapView).height(300)
+        $0.addItem(self.mapView).height(Metric.mapHeight)
       }
     
     self.view.flex.addItem()
       .direction(.column)
       .alignItems(.stretch)
-      .marginHorizontal(20)
+      .marginHorizontal(Metric.horizontalMargin)
       .define {
-        $0.addItem(self.nameLabel).height(50)
-        $0.addItem(self.nameTextField).height(40)
-        $0.addItem(self.descriptionLabel).height(50)
-        $0.addItem(self.descriptionTextField).height(40)
+        $0.addItem(self.nameLabel).height(Metric.labelHeight)
+        $0.addItem(self.nameTextField).height(Metric.textFieldHeight)
+        $0.addItem(self.descriptionLabel).height(Metric.labelHeight)
+        $0.addItem(self.descriptionTextField).height(Metric.textFieldHeight)
       }
   }
   
-  
-  override func viewDidLayoutSubviews() { //?
-    super.viewDidLayoutSubviews()
-    self.view.flex.layout(mode: .fitContainer)
-  }
+  /*
+   // MARK: Layout
+
+   private func defineFlexContainer() {
+     self.view.flex.direction(.column).define {
+       $0.addItem(self.mapView).height(Metric.mapHeight)
+
+       $0.addItem().direction(.column)
+         .alignItems(.stretch)
+         .marginHorizontal(Metric.horizontalMargin)
+         .define {
+           $0.addItem(self.nameLabel).height(Metric.labelHeight)
+           $0.addItem(self.nameTextField).height(Metric.textFieldHeight)
+           $0.addItem(self.descriptionLabel).height(Metric.labelHeight)
+           $0.addItem(self.descriptionTextField).height(Metric.textFieldHeight)
+         }
+     }
+   }
+*/
 }
