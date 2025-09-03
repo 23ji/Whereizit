9383766c115296ada9925dcf7133026e59bf21ee
23ji <isj4194@naver.com>
2025-09-03 17:22:07 +0900

    바텀 시트 초기화

--- SmokingAreaOdi/Controller/HomeViewController.swift
+++ SmokingAreaOdi/Controller/HomeViewController.swift
@@ -20,180 +20,181 @@ import UIKit
 
 
 final class HomeViewController: UIViewController {
+  
+  // MARK: Constant
+  
+  private enum Metric {
+    static let addButtonTrailing: CGFloat = 24
+    static let addButtonBottom: CGFloat = 40
+  }
+  
+  
+  // MARK: UI
+  
+  private let mapView = NMFNaverMapView()
+  private let addButton = UIButton().then {
+    $0.setImage(UIImage(named: "plusButton"), for: .normal)
+  }
+  
+  
+  // MARK: Property
+  
+  private let db = Firestore.firestore()
+  
+  private let locationManager = CLLocationManager()
+  
+  
+  // MARK: 바텀시트
+  
+  // TODO: 힌트 1
+  // FloatingPanelController를 담을 변수를 선언해주세요.
+  
+  
+  // TODO: 힌트 2
+  // 바텀시트에 띄울 ViewController의 인스턴스를 생성해주세요.
+  
+  
+  // MARK: Rx
+  
+  private let markerTapped = PublishSubject<SmokingArea>()
+  private let disposeBag = DisposeBag()
+  
+  
+  // MARK: LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.setup()
+    self.addSubviews()
+    self.makeConstraints()
     
-    // MARK: Constant
+    self.setLocationManager()
     
-    private enum Metric {
-        static let addButtonTrailing: CGFloat = 24
-        static let addButtonBottom: CGFloat = 40
+    self.didTappedAddButton()
+    self.smokingAreas()
+    
+    // TODO: 힌트 3
+    // 마커가 탭 되었을 때의 동작을 처리하는 bind 함수를 호출해주세요.
+    
+    // TODO: 힌트 4
+    // 바텀시트를 설정하고 초기화하는 함수를 호출해주세요.
+    
+    self.mapView.mapView.touchDelegate = self
+  }
+  
+  // MARK: Setup
+  
+  private func setup() { self.navigationItem.title = "Home" }
+  
+  private func addSubviews() {
+    self.view.addSubview(self.mapView)
+    self.view.addSubview(self.addButton)
+  }
+  
+  private func makeConstraints() {
+    self.mapView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    
+    self.addButton.snp.makeConstraints {
+      $0.trailing.equalToSuperview().inset(Metric.addButtonTrailing)
+      $0.bottom.equalToSuperview().inset(Metric.addButtonBottom)
     }
-    
-    
-    // MARK: UI
-    
-    private let mapView = NMFNaverMapView()
-    private let addButton = UIButton().then {
-        $0.setImage(UIImage(named: "plusButton"), for: .normal)
-    }
-    
-    
-    // MARK: Property
-    
-    private let db = Firestore.firestore()
-    
-    private let locationManager = CLLocationManager()
-    
-    
-    // MARK: 바텀시트
-    
-    // TODO: 힌트 1
-    // FloatingPanelController를 담을 변수를 선언해주세요.
-    
-    // TODO: 힌트 2
-    // 바텀시트에 띄울 ViewController의 인스턴스를 생성해주세요.
-    
-    
-    // MARK: Rx
-    
-    private let markerTapped = PublishSubject<SmokingArea>()
-    private let disposeBag = DisposeBag()
-    
-    
-    // MARK: LifeCycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.setup()
-        self.addSubviews()
-        self.makeConstraints()
+  }
+  
+  
+  // MARK: Action
+  
+  private func didTappedAddButton() {
+    self.addButton.rx.tap.subscribe(
+      onNext : { [weak self] in
+        let markerPositionSeletorVC = MarkerPositionSelectorViewController()
+        self?.navigationController?.pushViewController(markerPositionSeletorVC, animated: true)
+      })
+    .disposed(by: self.disposeBag)
+  }
+  
+  
+  // MARK: Area Marker
+  
+  private func smokingAreas() {
+    db.collection("smokingAreas").addSnapshotListener { snapshot, error in
+      guard let snapshot = snapshot else { return }
+      for doc in snapshot.documents {
+        let data = doc.data()
         
-        self.setLocationManager()
+        guard let name = data["name"] as? String,
+              let description = data["description"] as? String,
+              let areaLat = data["areaLat"] as? Double,
+              let areaLng = data["areaLng"] as? Double
+        else { return }
+        let selectedEnvironmentTags = (data["environmentTags"] as? [String]) ?? []
+        let selectedTypeTags = (data["typeTags"] as? [String]) ?? []
+        let selectedFacilityTags = (data["facilityTags"] as? [String]) ?? []
         
-        self.didTappedAddButton()
-        self.smokingAreas()
         
-        // TODO: 힌트 3
-        // 마커가 탭 되었을 때의 동작을 처리하는 bind 함수를 호출해주세요.
+        let areaData = SmokingArea(
+          name: name,
+          description: description,
+          areaLat: areaLat,
+          areaLng: areaLng,
+          selectedEnvironmentTags: selectedEnvironmentTags,
+          selectedTypeTags: selectedTypeTags,
+          selectedFacilityTags: selectedFacilityTags
+        )
         
-        // TODO: 힌트 4
-        // 바텀시트를 설정하고 초기화하는 함수를 호출해주세요.
+        let areaMarker = NMFMarker()
+        areaMarker.iconImage = NMFOverlayImage(name: "marker_Pin")
+        areaMarker.position = NMGLatLng(lat: areaLat, lng: areaLng)
         
-        self.mapView.mapView.touchDelegate = self
-    }
-    
-    // MARK: Setup
-    
-    private func setup() { self.navigationItem.title = "Home" }
-    
-    private func addSubviews() {
-        self.view.addSubview(self.mapView)
-        self.view.addSubview(self.addButton)
-    }
-    
-    private func makeConstraints() {
-        self.mapView.snp.makeConstraints { $0.edges.equalToSuperview() }
-        
-        self.addButton.snp.makeConstraints {
-            $0.trailing.equalToSuperview().inset(Metric.addButtonTrailing)
-            $0.bottom.equalToSuperview().inset(Metric.addButtonBottom)
+        areaMarker.touchHandler = { (overlay: NMFOverlay) -> Bool in
+          self.markerTapped.onNext(areaData)
+          return true
         }
+        areaMarker.mapView = self.mapView.mapView
+      }
     }
-    
-    
-    // MARK: Action
-    
-    private func didTappedAddButton() {
-        self.addButton.rx.tap.subscribe(
-            onNext : { [weak self] in
-                let markerPositionSeletorVC = MarkerPositionSelectorViewController()
-                self?.navigationController?.pushViewController(markerPositionSeletorVC, animated: true)
-            })
-        .disposed(by: self.disposeBag)
-    }
-    
-    
-    // MARK: Area Marker
-    
-    private func smokingAreas() {
-        db.collection("smokingAreas").addSnapshotListener { snapshot, error in
-            guard let snapshot = snapshot else { return }
-            for doc in snapshot.documents {
-                let data = doc.data()
-                
-                guard let name = data["name"] as? String,
-                      let description = data["description"] as? String,
-                      let areaLat = data["areaLat"] as? Double,
-                      let areaLng = data["areaLng"] as? Double
-                else { return }
-                let selectedEnvironmentTags = (data["environmentTags"] as? [String]) ?? []
-                let selectedTypeTags = (data["typeTags"] as? [String]) ?? []
-                let selectedFacilityTags = (data["facilityTags"] as? [String]) ?? []
-                
-                
-                let areaData = SmokingArea(
-                    name: name,
-                    description: description,
-                    areaLat: areaLat,
-                    areaLng: areaLng,
-                    selectedEnvironmentTags: selectedEnvironmentTags,
-                    selectedTypeTags: selectedTypeTags,
-                    selectedFacilityTags: selectedFacilityTags
-                )
-                
-                let areaMarker = NMFMarker()
-                areaMarker.iconImage = NMFOverlayImage(name: "marker_Pin")
-                areaMarker.position = NMGLatLng(lat: areaLat, lng: areaLng)
-                
-                areaMarker.touchHandler = { (overlay: NMFOverlay) -> Bool in
-                    self.markerTapped.onNext(areaData)
-                    return true
-                }
-                areaMarker.mapView = self.mapView.mapView
-            }
-        }
-    }
-    
-    // TODO: 힌트 5
-    // markerTapped 이벤트를 구독(subscribe)하여 처리하는 bind() 함수를 만들어주세요.
-    // 1. 마커에서 전달받은 SmokingArea 데이터로 바텀시트의 UI를 업데이트 해야합니다. (예: smokingAreaBottomSheetVC.configure(with:))
-    // 2. 숨겨져 있는 바텀시트를 위로 올려서 보여줘야 합니다. (예: floatingPanel.move(to: .half, animated: true))
-    
+  }
+  
+  // TODO: 힌트 5
+  // markerTapped 이벤트를 구독(subscribe)하여 처리하는 bind() 함수를 만들어주세요.
+  // 1. 마커에서 전달받은 SmokingArea 데이터로 바텀시트의 UI를 업데이트 해야합니다. (예: smokingAreaBottomSheetVC.configure(with:))
+  // 2. 숨겨져 있는 바텀시트를 위로 올려서 보여줘야 합니다. (예: floatingPanel.move(to: .half, animated: true))
+  
 }
 
 
 // MARK: Location / Camera
 // 위치 업데이트 시 최신 좌표로 카메라 이동
 extension HomeViewController: CLLocationManagerDelegate {
+  
+  private func setLocationManager() {
+    self.locationManager.delegate = self // 위치 정보 업데이트 시 델리게이트 호출
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation // 최고 수준 정확도
+    self.locationManager.distanceFilter = kCLDistanceFilterNone // 조금 변경되어도 위치 업데이트
+    self.locationManager.activityType = .otherNavigation // 도로 따르지 않는 수준의 사용자
+    self.locationManager.pausesLocationUpdatesAutomatically = false // 사용자가 움직이지 않아도 위치 업데이트
+    self.locationManager.requestWhenInUseAuthorization() // 위치 사용 권한 요청
+    self.locationManager.startUpdatingLocation() // 위치 업데이트 시작
+  }
+  
+  // CLLocationManagerDelegate
+  // 새로운 위치 데이터가 업데이트될 때 호출되는 델리게이트 메서드입니다.
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    // 1. locations 배열에서 가장 최근 위치 정보 가져오기
+    guard let bestLocation = locations.last else { return }
+    // 2. 현재 위치의 위도와 경도 추출
+    let userLat = bestLocation.coordinate.latitude
+    let userLng = bestLocation.coordinate.longitude
     
-    private func setLocationManager() {
-        self.locationManager.delegate = self // 위치 정보 업데이트 시 델리게이트 호출
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation // 최고 수준 정확도
-        self.locationManager.distanceFilter = kCLDistanceFilterNone // 조금 변경되어도 위치 업데이트
-        self.locationManager.activityType = .otherNavigation // 도로 따르지 않는 수준의 사용자
-        self.locationManager.pausesLocationUpdatesAutomatically = false // 사용자가 움직이지 않아도 위치 업데이트
-        self.locationManager.requestWhenInUseAuthorization() // 위치 사용 권한 요청
-        self.locationManager.startUpdatingLocation() // 위치 업데이트 시작
-    }
+    print("1. 사용자의 위치 : (\(userLat), \(userLng))")
     
-    // CLLocationManagerDelegate
-    // 새로운 위치 데이터가 업데이트될 때 호출되는 델리게이트 메서드입니다.
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        // 1. locations 배열에서 가장 최근 위치 정보 가져오기
-        guard let bestLocation = locations.last else { return }
-        // 2. 현재 위치의 위도와 경도 추출
-        let userLat = bestLocation.coordinate.latitude
-        let userLng = bestLocation.coordinate.longitude
-        
-        print("1. 사용자의 위치 : (\(userLat), \(userLng))")
-        
-        // 3. 지도 뷰를 현재 위치로 이동시키는 메서드 호출
-        self.cameraUpdate(lat: userLat, lng: userLng)
-    }
-    
-    private func cameraUpdate(lat: Double, lng: Double) {
-        let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
-        self.mapView.mapView.moveCamera(cameraUpdate)
-    }
+    // 3. 지도 뷰를 현재 위치로 이동시키는 메서드 호출
+    self.cameraUpdate(lat: userLat, lng: userLng)
+  }
+  
+  private func cameraUpdate(lat: Double, lng: Double) {
+    let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: lat, lng: lng))
+    self.mapView.mapView.moveCamera(cameraUpdate)
+  }
 }
 
 
@@ -207,8 +208,8 @@ extension HomeViewController: CLLocationManagerDelegate {
 
 
 extension HomeViewController: NMFMapViewTouchDelegate {
-    func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
-        // TODO: 힌트 7
-        // 지도를 탭했을 때, 화면에 보이는 바텀시트를 다시 숨겨주세요.
-    }
+  func mapView(_ mapView: NMFMapView, didTapMap latlng: NMGLatLng, point: CGPoint) {
+    // TODO: 힌트 7
+    // 지도를 탭했을 때, 화면에 보이는 바텀시트를 다시 숨겨주세요.
+  }
 }
--- SmokingAreaOdi/Controller/SmokingAreaBottomSheetViewController.swift
+++ SmokingAreaOdi/Controller/SmokingAreaBottomSheetViewController.swift
@@ -12,97 +12,97 @@ import Then
 import UIKit
 
 final class SmokingAreaBottomSheetViewController: UIViewController {
-    
-    // MARK:  UI Components
-    
-    private let placeImageView = UIImageView().then {
-        $0.backgroundColor = .systemGray5
-        $0.layer.cornerRadius = 8
-        $0.clipsToBounds = true
-        $0.contentMode = .scaleAspectFill
-    }
-    
-    private let nameLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 18, weight: .bold)
-        $0.textColor = .black
-        $0.text = "장소 이름"
-    }
-    
-    private let descriptionLabel = UILabel().then {
-        $0.font = .systemFont(ofSize: 14, weight: .regular)
-        $0.textColor = .systemGray
-        $0.text = "장소 설명 어쩌고 저쩌고"
-    }
-    
-    // 환경, 유형 등을 그룹으로 묶기 위한 UI들
-    private let environmentTitleLabel = createSectionTitleLabel(title: "환경")
-    private let environmentTagStackView = createTagStackView()
-    
-    private let typeTitleLabel = createSectionTitleLabel(title: "유형")
-    private let typeTagStackView = createTagStackView()
-    
-    // 전체 UI를 담을 메인 스택뷰
-    private let mainStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.spacing = 20
-        $0.alignment = .fill
-        $0.distribution = .fill
-    }
-
-    // MARK: - LifeCycle
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        setupLayout()
-    }
-    
-    // MARK: - Setup Layout
-    
-    private func setupLayout() {
-        // TODO: 힌트 1
-        // UI 컴포넌트들을 스택뷰(StackView)를 사용하여 그룹화하고 레이아웃을 설정해주세요.
-        // 1. 장소 이름(nameLabel)과 설명(descriptionLabel)을 묶는 수직 스택뷰를 만드세요.
-        // 2. 장소 이미지(placeImageView)와 1번에서 만든 스택뷰를 묶는 수평 스택뷰를 만드세요. (정보 섹션)
-        // 3. 환경 타이틀(environmentTitleLabel)과 환경 태그 스택뷰(environmentTagStackView)를 묶는 수직 스택뷰를 만드세요. (환경 섹션)
-        // 4. 유형 타이틀(typeTitleLabel)과 유형 태그 스택뷰(typeTagStackView)를 묶는 수직 스택뷰를 만드세요. (유형 섹션)
-        // 5. 위에서 만든 모든 섹션(2, 3, 4)을 메인 스택뷰(mainStackView)에 추가하세요.
-        // 6. 마지막으로, view에 mainStackView를 추가하고 SnapKit을 사용해 오토레이아웃 제약조건을 설정해주세요.
-    }
-    
-    // MARK: - Public Method
-    
-    public func configure(with data: SmokingArea) {
-        // TODO: 힌트 2
-        // HomeViewController에서 전달받은 SmokingArea 데이터로 UI를 업데이트 해주세요.
-        // 1. nameLabel과 descriptionLabel의 text를 설정해주세요.
-        // 2. 태그 스택뷰들(environmentTagStackView, typeTagStackView)에 기존 태그가 남아있을 수 있으니, 모두 제거해주세요. (재사용 대비)
-        // 3. data에 있는 태그 배열(selectedEnvironmentTags, selectedTypeTags)을 반복문으로 돌면서,
-        //    createTagLabel(text:) 헬퍼 메서드를 사용해 태그 라벨을 생성하고, 각 스택뷰에 추가해주세요.
-    }
-    
-   
-    // MARK:  Helper Methods (UI 생성)
-    
-    private static func createSectionTitleLabel(title: String) -> UILabel {
-        // TODO: 힌트 3
-        // 섹션의 제목(환경, 유형 등)으로 사용할 UILabel을 생성하고 설정하여 반환해주세요.
-        // (글자 크기, 굵기, 색상 등)
-        return UILabel() // 임시 반환
-    }
-    
-    private static func createTagStackView() -> UIStackView {
-        // TODO: 힌트 4
-        // 태그들을 담을 수평 UIStackView를 생성하고 설정하여 반환해주세요.
-        // (방향, 간격 등)
-        return UIStackView() // 임시 반환
-    }
-
-    private func createTagLabel(text: String) -> UILabel {
-        // TODO: 힌트 5
-        // "실내", "의자 있음" 등 개별 태그로 사용할 UILabel을 생성하고 스타일을 적용하여 반환해주세요.
-        // (배경색, 글자색, 모서리 둥글게 등)
-        return UILabel() // 임시 반환
-    }
+  
+  // MARK:  UI Components
+  
+  private let placeImageView = UIImageView().then {
+    $0.backgroundColor = .systemGray5
+    $0.layer.cornerRadius = 8
+    $0.clipsToBounds = true
+    $0.contentMode = .scaleAspectFill
+  }
+  
+  private let nameLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 18, weight: .bold)
+    $0.textColor = .black
+    $0.text = "장소 이름"
+  }
+  
+  private let descriptionLabel = UILabel().then {
+    $0.font = .systemFont(ofSize: 14, weight: .regular)
+    $0.textColor = .systemGray
+    $0.text = "장소 설명 어쩌고 저쩌고"
+  }
+  
+  // 환경, 유형 등을 그룹으로 묶기 위한 UI들
+  private let environmentTitleLabel = createSectionTitleLabel(title: "환경")
+  private let environmentTagStackView = createTagStackView()
+  
+  private let typeTitleLabel = createSectionTitleLabel(title: "유형")
+  private let typeTagStackView = createTagStackView()
+  
+  // 전체 UI를 담을 메인 스택뷰
+  private let mainStackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 20
+    $0.alignment = .fill
+    $0.distribution = .fill
+  }
+  
+  // MARK: - LifeCycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    setupLayout()
+  }
+  
+  // MARK: - Setup Layout
+  
+  private func setupLayout() {
+    // TODO: 힌트 1
+    // UI 컴포넌트들을 스택뷰(StackView)를 사용하여 그룹화하고 레이아웃을 설정해주세요.
+    // 1. 장소 이름(nameLabel)과 설명(descriptionLabel)을 묶는 수직 스택뷰를 만드세요.
+    // 2. 장소 이미지(placeImageView)와 1번에서 만든 스택뷰를 묶는 수평 스택뷰를 만드세요. (정보 섹션)
+    // 3. 환경 타이틀(environmentTitleLabel)과 환경 태그 스택뷰(environmentTagStackView)를 묶는 수직 스택뷰를 만드세요. (환경 섹션)
+    // 4. 유형 타이틀(typeTitleLabel)과 유형 태그 스택뷰(typeTagStackView)를 묶는 수직 스택뷰를 만드세요. (유형 섹션)
+    // 5. 위에서 만든 모든 섹션(2, 3, 4)을 메인 스택뷰(mainStackView)에 추가하세요.
+    // 6. 마지막으로, view에 mainStackView를 추가하고 SnapKit을 사용해 오토레이아웃 제약조건을 설정해주세요.
+  }
+  
+  // MARK: - Public Method
+  
+  public func configure(with data: SmokingArea) {
+    // TODO: 힌트 2
+    // HomeViewController에서 전달받은 SmokingArea 데이터로 UI를 업데이트 해주세요.
+    // 1. nameLabel과 descriptionLabel의 text를 설정해주세요.
+    // 2. 태그 스택뷰들(environmentTagStackView, typeTagStackView)에 기존 태그가 남아있을 수 있으니, 모두 제거해주세요. (재사용 대비)
+    // 3. data에 있는 태그 배열(selectedEnvironmentTags, selectedTypeTags)을 반복문으로 돌면서,
+    //    createTagLabel(text:) 헬퍼 메서드를 사용해 태그 라벨을 생성하고, 각 스택뷰에 추가해주세요.
+  }
+  
+  
+  // MARK:  Helper Methods (UI 생성)
+  
+  private static func createSectionTitleLabel(title: String) -> UILabel {
+    // TODO: 힌트 3
+    // 섹션의 제목(환경, 유형 등)으로 사용할 UILabel을 생성하고 설정하여 반환해주세요.
+    // (글자 크기, 굵기, 색상 등)
+    return UILabel() // 임시 반환
+  }
+  
+  private static func createTagStackView() -> UIStackView {
+    // TODO: 힌트 4
+    // 태그들을 담을 수평 UIStackView를 생성하고 설정하여 반환해주세요.
+    // (방향, 간격 등)
+    return UIStackView() // 임시 반환
+  }
+  
+  private func createTagLabel(text: String) -> UILabel {
+    // TODO: 힌트 5
+    // "실내", "의자 있음" 등 개별 태그로 사용할 UILabel을 생성하고 스타일을 적용하여 반환해주세요.
+    // (배경색, 글자색, 모서리 둥글게 등)
+    return UILabel() // 임시 반환
+  }
 }
 
