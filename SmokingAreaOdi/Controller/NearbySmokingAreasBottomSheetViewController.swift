588454ef9a81bf27b1f96e365725fba2abca87ff
23ji <isj4194@naver.com>
2025-09-15 19:36:27 +0900

    titleLabel 추가 및 레이아웃 구성

--- SmokingAreaOdi/Controller/NearbySmokingAreasBottomSheetViewController.swift
+++ SmokingAreaOdi/Controller/NearbySmokingAreasBottomSheetViewController.swift
@@ -5,36 +5,42 @@
 //  Created by 이상지 on 9/15/25.
 //
 
-import FirebaseCore
-import FirebaseStorage
+import UIKit
 import FlexLayout
 import PinLayout
 import Then
 
-import UIKit
-
 final class NearbySmokingAreasBottomSheetViewController: UIViewController {
   
-  let titleLabel = UILabel().then {
+  private let rootContainer = UIView()
+  
+  private let titleLabel = UILabel().then {
     $0.text = "주변 흡연구역 목록"
-    $0.font = .systemFont(ofSize: 20, weight: .bold)
+    $0.font = .systemFont(ofSize: 15, weight: .light)
+    $0.textAlignment = .center
   }
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.view.backgroundColor = .white
+    self.view.addSubview(rootContainer)
     self.setupLayout()
   }
   
   
-  private func addSubviews() {
-    self.view.addSubview(self.titleLabel)
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    self.rootContainer.pin.all()
+    self.rootContainer.flex.layout()
   }
   
   
   private func setupLayout() {
-    self.view.flex.direction(.column).define {
-      $0.addItem(self.titleLabel).width(100).height(30).margin(10, 10, 10, 10)
-    }
+    self.rootContainer.flex.justifyContent(.start).marginTop(30)
+      .alignItems(.center)
+      .define {
+        $0.addItem(titleLabel)
+      }
   }
 }
