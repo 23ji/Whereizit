b18b8e9a7d871c8baa6672e37a249e83bd0c796e
23ji <isj4194@naver.com>
2025-09-15 19:03:27 +0900

    self 누락 수정

--- SmokingAreaOdi/Controller/NearbySmokingAreasBottomSheetViewController.swift
+++ SmokingAreaOdi/Controller/NearbySmokingAreasBottomSheetViewController.swift
@@ -19,8 +19,22 @@ final class NearbySmokingAreasBottomSheetViewController: UIViewController {
     $0.text = "주변 흡연구역 목록"
     $0.font = .systemFont(ofSize: 20, weight: .bold)
   }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    self.setupLayout()
+  }
+  
+  
+  private func addSubviews() {
+    self.view.addSubview(self.titleLabel)
+  }
+  
+  
+  private func setupLayout() {
+    self.view.flex.direction(.column).define {
+      $0.addItem(self.titleLabel).width(100).height(30).margin(10, 10, 10, 10)
+    }
   }
 }
