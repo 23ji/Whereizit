9e68bae32d50e3306ab3b2fb88c4aeb6bf62778f
23ji <isj4194@naver.com>
2025-09-16 19:56:55 +0900

    카카오톡으로 로그인을 위한 설정

---
+++ SmokingAreaOdi/Controller/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  SmokingAreaOdi
+//
+//  Created by 23ji on 9/16/25.
+//
+
+import UIKit
+
+final class LoginViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
