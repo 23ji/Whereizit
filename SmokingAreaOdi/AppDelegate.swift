fd2ae5a0f84b8de0aad09e055bfb692b59c1f7f7
23ji <isj4194@naver.com>
2025-09-23 08:42:36 +0900

    구글 로그인 초기 세팅

--- SmokingAreaOdi/AppDelegate.swift
+++ SmokingAreaOdi/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import FirebaseCore
 import FirebaseFirestore
+import GoogleSignIn
 import IQKeyboardManagerSwift
 import KakaoSDKCommon
 import KakaoSDKAuth
@@ -18,11 +19,15 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
   
   func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    if (AuthApi.isKakaoTalkLoginUrl(url)) {
+    // Kakao 로그인 처리
+    if AuthApi.isKakaoTalkLoginUrl(url) {
       return AuthController.handleOpenUrl(url: url)
     }
-    return false
-  }
+    
+    // Google 로그인 처리
+    if GIDSignIn.sharedInstance.handle(url) {
+      return true
+    }
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
