a678d9d4b9b51670acac84dc07e0dd7295d10bac
23ji <isj4194@naver.com>
2025-10-03 15:23:23 +0900

    SmokingAreaTableViewCell 생성

---
+++ SmokingAreaTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  SmokingAreaTableViewCell.swift
+//  SmokingAreaOdi
+//
+//  Created by 23ji on 10/3/25.
+//
+
+import FlexLayout
+import PinLayout
+import Then
+
+import UIKit
+
+
+final class SmokingAreaTableViewCell: UITableViewCell {
+  
+  
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    // Initialization code
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+    
+    // Configure the view for the selected state
+  }
+  
+}
