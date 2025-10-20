ed50e776325724f2add945327d57bf8906758b05
23ji <isj4194@naver.com>
2025-10-20 11:46:38 +0900

    MySmokingAreasViewController 생성

---
+++ SmokingAreaOdi/Controller/MySmokingAreasViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MySmokingAreasViewController.swift
+//  SmokingAreaOdi
+//
+//  Created by 이상지 on 10/20/25.
+//
+
+import UIKit
+
+final class MySmokingAreasViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
