b644b27fe641109d16054636a64435be8a0f26c7
23ji <isj4194@naver.com>
2025-11-26 19:54:53 +0900

    데이터 저장 로직 분리를 위한 AreaRepository 생성

---
+++ Whereizit/Service/AreaRepository.swift
@@ -0,0 +1,36 @@
+//
+//  AreaRepository.swift
+//  Whereizit
+//
+//  Created by 23ji on 11/26/25.
+//
+
+import RxSwift
+import FirebaseFirestore
+import FirebaseStorage
+
+final class AreaRepository {
+  static let shared = AreaRepository()
+  private let db = Firestore.firestore()
+
+  private let collectionPath = "smokingArea"
+
+  func addArea(area: Area) -> Observable<Void> {
+    return Observable.create { [weak self] observer in
+      guard let self = self, let documentID = area.documentID else {
+        observer.onError(NSError(domain: "InvalidData", code: 400, userInfo: nil))
+        return Disposables.create()
+      }
+
+      self.db.collection(self.collectionPath).document(documentID).setData(area.asDictionary) { error in
+        if let error = error {
+          observer.onError(error)
+        } else {
+          observer.onNext(())
+          observer.onCompleted()
+        }
+      }
+      return Disposables.create()
+    }
+  }
+}
