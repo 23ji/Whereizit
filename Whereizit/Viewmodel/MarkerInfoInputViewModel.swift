c27da0c6ba3666fa154b79c9cb1dd20415de7afc
23ji <isj4194@naver.com>
2025-12-12 13:55:01 +0900

    refactor : MarkerInfoInputViewModel 생성

---
+++ Whereizit/Viewmodel/MarkerInfoInputViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  MarkerInfoInputViewModel.swift
+//  Whereizit
+//
+//  Created by 이상지 on 12/12/25.
+//
+
+import Foundation
+
+final class MarkerInfoInputViewModel {
+  struct Input {
+    let name: BehaviorRelay<String>
+    let description: BehaviorRelay<String>
+    let selectedCategory: BehaviorRelay<String?>
+    let environmentTags: BehaviorRelay<[String]>
+    let typeTags: BehaviorRelay<[String]>
+    let facilityTags: BehaviorRelay<[String]>
+    let saveTapped: PublishRelay<Void>
+  }
+  
+  struct Output {
+    let isSaveEnabled: Driver<Bool>
+    let tagsForSection: Driver<[String: [String]]>
+    let saveResult: PublishRelay<Bool>
+  }
+}
