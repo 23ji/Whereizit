4512ff7473ffdb2ada577947c640beb42bc52322
23ji <isj4194@naver.com>
2025-12-21 17:25:53 +0900

    RxSwift 공부 내용 정리

--- Whereizit/Viewmodel/MarkerInfoInputViewModel.swift
+++ Whereizit/Viewmodel/MarkerInfoInputViewModel.swift
@@ -10,17 +10,17 @@ import RxRelay
 import RxCocoa
 
 final class MarkerInfoInputViewModel {
-  struct Input {
-    let name: BehaviorRelay<String>
+  struct Input { // View -> ViewModel
+    let name: BehaviorRelay<String> // BehaviorRelay : 초기값을 가지며 항상 최신값을 유지
     let description: BehaviorRelay<String>
     let selectedCategory: BehaviorRelay<String?>
     let environmentTags: BehaviorRelay<[String]>
     let typeTags: BehaviorRelay<[String]>
     let facilityTags: BehaviorRelay<[String]>
-    let saveTapped: PublishRelay<Void>
+    let saveTapped: PublishRelay<Void> // PublishRelay : 구독 이후 이벤트만 처리하며 종료되지 않음
   }
   
-  struct Output {
+  struct Output { // ViewModel -> View
     let isSaveEnabled: Driver<Bool>
     let tagsForSection: Driver<[String: [String]]>
     let saveResult: PublishRelay<Bool>
